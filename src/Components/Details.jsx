b74cfc769918cc5a5d7b863ea620cc949d535b0e
inestell <ines_tellechea@hotmail.com>
2024-06-27 20:44:02 +0100

    better details

--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -1,6 +1,8 @@
 import { useContext, useEffect} from 'react';
 import { MyContext } from './Context';
 import { useParams } from 'react-router-dom';
+import { faCakeCandles, faPerson, faFilm, faShuttleSpace, faVanShuttle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Details () {
     const {data, fetchData} = useContext(MyContext);
@@ -15,26 +17,65 @@ function Details () {
 
     return (
         <>
-            <div className="p-3" style={{backgroundColor: "#fbb03b"}}>
-                <h4 style={{fontSize: "40px"}}>{thisPerson.name}</h4>
-            </div>
             <div className="row">
-                <img className="col-6" src={thisPerson.img} />
-                <div className="col-6">
-                    <ul style={{listStyleType: "none"}}>
-                        <li>Date of birth: {thisPerson.birth_year}</li>
-                        <li>Planet of birth: </li>
-                        <li>Gender: {thisPerson.gender}</li>
-                        <li>Skin Color: {thisPerson.skin_color}</li>
-                        <li>Eye Color: {thisPerson.eye_color}</li>
-                        <li>Hair Color: {thisPerson.hair_color}</li>
-                        <li>Height: {thisPerson.height}</li>
-                        <li>Mass: {thisPerson.mass}</li>
-                        <li>Films: </li>
-                        <li>Starships: </li>
-                        <li>Vehicles: </li>
-                    </ul>
+                <div className="col-6 p-5">
+                    <h4 style={{fontSize: "80px", color: "#fbb03b", marginBottom: "50px"}}
+                        className="divider">{thisPerson.name}</h4>
+                    <div>
+                        <h6>
+                            <span style={{padding: "8px", color: "#fbb03b"}}>
+                                <FontAwesomeIcon icon={ faCakeCandles} />
+                            </span>
+                            Birth
+                        </h6>
+                        <ul className="mb-5">
+                            <li>Date of birth: {thisPerson.birth_year}</li>
+                            <li>Planet of birth: </li>
+                        </ul>
+                        <h6>
+                            <span style={{padding: "8px", color: "#fbb03b"}}>
+                                <FontAwesomeIcon icon={ faPerson } />
+                            </span>
+                            Personal
+                        </h6>
+                        <ul className="mb-5">
+                            <li>Gender: {thisPerson.gender}</li>
+                            <li>Skin Color: {thisPerson.skin_color}</li>
+                            <li>Eye Color: {thisPerson.eye_color}</li>
+                            <li>Hair Color: {thisPerson.hair_color}</li>
+                            <li>Height: {thisPerson.height}</li>
+                            <li>Mass: {thisPerson.mass}</li>
+                        </ul>
+                        <h6>
+                            <span style={{padding: "8px", color: "#fbb03b"}}>
+                                <FontAwesomeIcon icon={ faFilm} />
+                            </span>
+                            Films
+                        </h6>
+                        <ul className="mb-5">
+                            <li>Films: </li>
+                        </ul>
+                        <h6>
+                            <span style={{padding: "8px", color: "#fbb03b"}}>
+                                <FontAwesomeIcon icon={ faShuttleSpace} />
+                            </span>
+                            Starships
+                        </h6>
+                        <ul className="mb-5">
+                            <li>Starships: </li>
+                        </ul>
+                        <h6>
+                            <span style={{padding: "8px", color: "#fbb03b"}}>
+                                <FontAwesomeIcon icon={ faVanShuttle} />
+                            </span>
+                            Vehicles
+                        </h6>
+                        <ul className="mb-5">
+                            <li>Vehicles: </li>
+                        </ul>
+                    </div>
                 </div>
+                <img className="col-6" src={thisPerson.img} />
             </div>
         </>
     )
